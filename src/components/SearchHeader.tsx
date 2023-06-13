4a2bb7ae1a538df126e3776aefa85d95326bdd43
jinwook song <wlsdnr129@naver.com>
2023-06-13 18:49:06 +0900

    components: search header

---
+++ src/components/SearchHeader.tsx
@@ -0,0 +1,39 @@
+import { FormEvent, useEffect, useState } from 'react';
+import { BsSearch, BsYoutube } from 'react-icons/bs';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
+export default function SearchHeader() {
+  const { keyword } = useParams();
+  const navigate = useNavigate();
+  const [text, setText] = useState('');
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    navigate(`/videos/${text}`);
+  }
+
+  useEffect(() => {
+    setText(keyword ?? '');
+  }, [keyword]);
+
+  return (
+    <header className='w-full flex p-4 text-2xl border-b border-zinc-600 mb-4'>
+      <Link to={'/'} className='flex items-center gap-x-2'>
+        <BsYoutube className='text-4xl text-brand' />
+        <h1 className='font-semibold text-3xl'>Youtube</h1>
+      </Link>
+      <form className='w-full flex justify-center' onSubmit={handleSubmit}>
+        <input
+          className='w-7/12 p-2 outline-none bg-black text-gray-50'
+          type='text'
+          placeholder='Search...'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button className='bg-zinc-600 px-6'>
+          <BsSearch />
+        </button>
+      </form>
+    </header>
+  );
+}
