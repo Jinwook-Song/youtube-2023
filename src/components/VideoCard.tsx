6b148faa7237b15f00ddc6f98cabd8becb58c794
jinwook song <wlsdnr129@naver.com>
2023-06-15 22:36:35 +0900

    feat: video data & timeago

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,11 +1,37 @@
+import { formatAgo } from '../util/date';
+
+type ThumbnailType = {
+  url: string;
+  width: number;
+  height: number;
+};
+
 type VideoSnippetType = {
   title: string;
+  publishedAt: string;
+  thumbnails: {
+    default: ThumbnailType;
+    medium: ThumbnailType;
+    high: ThumbnailType;
+  };
+  channelTitle: string;
 };
 
 export type VideoCardType = {
   id: string;
   snippet: VideoSnippetType;
 };
+
 export default function VideoCard({ snippet }: VideoCardType) {
-  return <div>{snippet.title}</div>;
+  const { title, thumbnails, channelTitle, publishedAt } = snippet;
+  return (
+    <li>
+      <img src={thumbnails.medium.url} alt={title} />
+      <div>
+        <p>{title}</p>
+        <p>{channelTitle}</p>
+        <p>{formatAgo(publishedAt, 'ja')}</p>
+      </div>
+    </li>
+  );
 }
